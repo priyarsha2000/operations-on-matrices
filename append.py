b847501a881ef5ebc2cb2ec19cf7c6fd0bd94d18
priyarsha2000 <59010849+priyarsha2000@users.noreply.github.com>
2019-12-18 19:20:57 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.array(input('enter the array:'));
+b=np.array(input('enter the array:'));
+print 'the first matrix a\n',a
+print 'the second matrix b\n',b
+c=np.append(a,b)
+print'append two numbers:\n',c
+
